6e896586ddc56cc35273419e8764102a8465c7c9
PavanGitHub1990 <67890937+pavankumar9740@users.noreply.github.com>
2024-04-28 21:50:57 +0530

    webdriver update

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import os
 from time import sleep
-
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+# driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+# firefox_service = FirefoxService(executable_path=GeckoDriverManager().install())
+# driver=webdriver.Firefox(service=firefox_service)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://www.makemytrip.com/")
 driver.implicitly_wait(2)
 driver.maximize_window()
+# driver.implicitly_wait(5)
+# driver.find_element(By.XPATH,"//span[@class='commonModal__close']").click()
+# driver.implicitly_wait(2)
+# driver.switch_to.frame(driver.find_element(By.XPATH,"//iframe[@id='webklipper-publisher-widget-container-notification-frame']"))
+# driver.implicitly_wait(5)
+# driver.find_element(By.XPATH,"//i[@class='wewidgeticon we_close']").click()
+# driver.implicitly_wait(5)
+try:
+    iframe = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//iframe[@id='webklipper-publisher-widget-container-notification-frame']"))
+    )
+    driver.switch_to.frame(iframe)
+    driver.find_element(By.XPATH,"//i[@class='wewidgeticon we_close']").click()
+except Exception as e:
+    print("An error occurred:", e)
+# driver.switch_to.default_content()
 driver.find_element(By.XPATH,"//span[@class='commonModal__close']").click()
 driver.implicitly_wait(10)
 driver.find_element(By.ID,'fromCity').click()
